e9a9bdbdd3cbf92c13ec7d729e3485845b747e44
Tsapiv Volodymyr <volodja.tsv@gmail.com>
2022-01-27 10:22:50 +0200

    Tune monitor

--- classify.py
+++ classify.py
@@ -5,32 +5,35 @@ import numpy as np
 
 from fabric import make_datamodule
 from google_doodle_dataset import GoogleDoodleDataModule
-from triplets import ResNet18Encoder, mAP, clustering_accuracy
+from hardnet import HardNet
+from triplets import ResNet18Encoder
+from utils import mAP, clustering_accuracy
 from utils import parse_config
 from sklearn.manifold import TSNE
 import plotly.express as px
-from vae.model import WrappedVAE
+from vae import WrappedVAE
 
 MODELS = {
     'vea': WrappedVAE,
-    'cnn': ResNet18Encoder
+    'cnn': ResNet18Encoder,
+    'hardnet': HardNet
 }
 
 if __name__ == '__main__':
     config = parse_config()
-    vae = MODELS[config['model_type']].load_from_checkpoint(checkpoint_path=config['checkpoint_path'],
-                                                            map_location='cpu')
-    vae.eval()
+    model = MODELS[config['model_type']].load_from_checkpoint(checkpoint_path=config['checkpoint_path'],
+                                                              map_location='cpu')
+    model.eval()
     classes = np.array(list(json.load(open(f"{config['data_path']}/classes.json")).values()))
     batch_size = config['batch_size']
     datamodule: GoogleDoodleDataModule = make_datamodule(config['type'], config['data_path'], batch_size=batch_size)
     datamodule.setup()
-    loader = datamodule.test_dataloader()
+    loader = datamodule.train_dataloader()
     data, labels = next(iter(loader))
-    X = vae.encoder(data).detach()
+    X = model(data).detach()
 
     print("mAP:", mAP(X, labels))
-    print("Clustering:", clustering_accuracy(vae.encoder, loader, 'cpu', nth=1))
+    print("Clustering:", clustering_accuracy(model, loader, 'cpu', nth=1))
     X_emb = TSNE(n_components=3, learning_rate='auto', init='random').fit_transform(X.detach().numpy())
     fig = px.scatter_3d(x=X_emb[:, 0], y=X_emb[:, 1], z=X_emb[:, 2], color=classes[labels.detach().numpy()])
     fig.update_traces(marker_size=5)
--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,4 @@ from .dataset import CentralPad, channel_expander
 from .parse_config import parse_config
 from .model_saver import ModelSaver
 from .default_transforms import default_transform
-from .metrics import mAP
+from .metrics import mAP, clustering_accuracy
